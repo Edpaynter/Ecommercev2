ca4af0efec6ae2f49df0d436d3e6a8e183e54200
edpaynter <edisonxpaynter@gmail.com>
2021-01-10 20:46:05 -0500

    updated port

--- ui/server.js
+++ ui/server.js
@@ -20,6 +20,6 @@ app.get('/', function (req, res) {
 });
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
-console.log("listening on port")
+console.log("listening on port ")
