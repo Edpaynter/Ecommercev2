bbbbe9f67c1f187468e59ca85a37a5b6596741cb
edpaynter <edisonxpaynter@gmail.com>
2021-05-18 17:48:03 -0400

    updated email template

--- controllers/contactFormController.js
+++ controllers/contactFormController.js
@@ -1,9 +1,8 @@
 "use strict";
-require('dotenv').config()
-
+require("dotenv").config();
 
 const mongoose = require("mongoose");
-const ContactForm = require('../models/ContactForm');
+const ContactForm = require("../models/ContactForm");
 const transporter = require("../config/transporter");
 exports.list_all_forms = (req, res) => {
   ContactForm.find({}, (err, form) => {
@@ -23,9 +22,53 @@ exports.create_a_form = (req, res) => {
       to: `${process.env.COMPANY_EMAIL}`, // list of receivers
       subject: `${form.name} has reached out for more information`, // Subject line
       text: `${form.message}`, // plain text body
-      html: `<h3>Contact Email: ${form.email}</h3><br><h4>Name:${form.name}<h4>Message:</h4>${form.message}`, // html body
+      html: `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+      </head>
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+  
+      <body>
+          <div style="background-color: red;
+          color: white;
+          text-align: center;
+          letter-spacing: 5px;
+          text-transform: uppercase;">Do Not Reply to this Email</div>
+          <div>
+              <img>
+          </div>
+          <div>AllSides Email Delivery Service</div>
+          <hr style="background-color: black;
+          height: 2px;
+          font-family: 'Roboto', sans-serif;
+          ">
+          <div> From:<span> ${form.email} </span></div>
+          <div>
+          Name: ${form.name}
+          </div>
+          <div>
+              Message:  ${form.message}
+          </div>
+          <hr>
+          <div>
+              <small style="font-size: 10px;">
+                  Disclaimer: Any email sent this address will not be viewed or replied to, please copy the email
+                  above into a new thread to respond.
+              </small>
+          </div>
+      </body>
+      </html>`,
+      // html: `<h3>Contact Email: ${form.email}</h3><br><h4>Name:${form.name}<h4>Message:</h4>${form.message}`, // html body
     });
-  })
+  });
 };
 
 exports.read_a_form = (req, res) => {
