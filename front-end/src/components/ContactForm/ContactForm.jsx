c0027e92eef485af982b9d14accea28e003dca2b
edpaynter <edisonxpaynter@gmail.com>
2021-07-31 22:15:13 -0400

    updated package.json removed unused methods

--- front-end/src/components/ContactForm/ContactForm.jsx
+++ front-end/src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import { makeStyles } from "@material-ui/core/styles";
@@ -9,14 +9,6 @@ function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
 
 const ContactForm = () => {
   const [name, setName] = useState("");
