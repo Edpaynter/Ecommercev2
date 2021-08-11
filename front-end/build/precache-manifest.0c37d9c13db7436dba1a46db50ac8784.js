625a5d111c56cc488ae75d9ee0114653a78adee5
edpaynter <edisonxpaynter@gmail.com>
2021-08-11 10:43:21 -0400

    update build files

---
+++ front-end/build/precache-manifest.0c37d9c13db7436dba1a46db50ac8784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac9830a5accaac2f56464314949c016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6818ed50fe6e8d215a87",
+    "url": "/static/js/2.9a8cc2c9.chunk.js"
+  },
+  {
+    "revision": "5c0eb14f6adce81adc82242314745973",
+    "url": "/static/js/2.9a8cc2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0fac01cfad97b613fc",
+    "url": "/static/js/main.4a4ba373.chunk.js"
+  },
+  {
+    "revision": "673d8ffee3e8ed730e63",
+    "url": "/static/js/runtime-main.a0a66673.js"
+  }
+]);
