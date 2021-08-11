c06674890a9df96f606e8564ed27bfa1c02a42e5
edpaynter <edisonxpaynter@gmail.com>
2021-08-11 10:38:01 -0400

    work in progress

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const { MONGODB_URI } = require("./config");
-const userRoutes = require("./routes/user");
 const contactFormRoutes = require("./routes/contactFormRoutes");
-const appointmentSchedulerRoutes = require('./routes/appointmentSchedulerRoutes');
+
 const app = express();
 app.use(express.json());
 
@@ -23,7 +22,6 @@ app.use((req, res, next) => {
 
 // Register Route
 app.use(contactFormRoutes);
-app.use(appointmentSchedulerRoutes);
 
 // Error Handler
 app.use((error, req, res, next) => {
